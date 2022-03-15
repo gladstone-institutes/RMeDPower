be9d34941a01b14e30029075b1c23bf2f8ac8cf5
Min-Gyoung Shin <mushrumrum@gmail.com>
2022-03-15 16:56:12 -0700

    calculate_lmer_estimates.R added

---
+++ R/calculate_lmer_estimates.R
@@ -0,0 +1,118 @@
+#' @title calculate_lmer_estimates
+#'
+#'
+#' @description This function performs a linear mixed model analysis using lmer.
+#'
+#' Note: The current version does not accept categorical response variables, sample size parameters smaller than the observed samples size
+#'
+#' @import multtest
+#' @import simr
+#' @import lme4
+#' @import lmerTest
+#' @import readxl
+#'
+#' @param data Input data
+#' @param condition_column Name of the condition variable (ex variable with values such as control/case). The input file has to have a corresponding column name
+#' @param experimental_columns Name of variables related to experimental design such as "experiment", "plate", and "cell_line".
+#' @param response_column Name of the variable observed by performing the experiment. ex) intensity.
+#' @param condition_is_categorical Specify whether the condition variable is categorical. TRUE: Categorical, FALSE: Continuous.
+#' @param response_is_categorical Default: the observed variable is continuous Categorical response variable will be implemented in the future. TRUE: Categorical , FALSE: Continuous (default).
+#' @param target_columns Name of the experimental parameters to use for the power calculation.
+#' @param output Output file name
+#' @return A linear mixed model result
+#'
+#' @export
+#' @examples
+
+calculate_lmer_estimates <- function(data, condition_column, experimental_columns, response_column, target_columns, condition_is_categorical,
+                            response_is_categorical=FALSE, output=NULL){
+  
+  
+  
+  ######input error handler
+  if(!condition_column%in%colnames(data)){ print("condition_column should be one of the column names");return(NULL) }
+  if(sum(experimental_columns%in%colnames(data))!=length(experimental_columns) ){ print("experimental_columns must match column names");return(NULL) }
+  if(!response_column%in%colnames(data)){  print("response_column should be one of the column names");return(NULL) }
+  if(response_is_categorical==TRUE){ print("response_is_categorical is TRUE. Categorical response variable is not accepted in the current version");return(NULL) }
+  if(is.null(condition_is_categorical) | !condition_is_categorical%in%c(TRUE,FALSE)){ print("condition_is_categorical must be TRUE or FALSE");return(NULL) }
+  if(sum(target_columns%in%colnames(data))!=length(target_columns) ){ print("target_columns must match column names");return(NULL) }
+  
+  
+  
+  ####### remove empty lines
+  Data <- data[complete.cases(data),]
+  
+  cat("\n")
+  print("__________________________________________________________________Summary of data:")
+  print(summary(Data))
+  cat("\n")
+  
+  colnames_original=colnames(Data)
+  experimental_columns_index=NULL
+  ####### assign categorical variables
+  if(condition_is_categorical==TRUE) Data[,condition_column]=as.factor(Data[,condition_column])
+  if(response_is_categorical==TRUE) {
+    cat("\n")
+    print("_________________________________Categorical response variable is not accepted in the current version")}
+  cat("\n")
+  
+  
+  for(i in 1:length(experimental_columns)){
+    Data[,experimental_columns[i]]=as.factor(Data[,experimental_columns[i]])
+    experimental_columns_index=c(experimental_columns_index,which(colnames(Data)==experimental_columns[i]))
+    colnames(Data)[experimental_columns_index[i]]=paste("experimental_column",i,sep="")
+    
+    
+    cat("\n")
+    print(paste("_________________________________",experimental_columns[i]," is assigned to experimental_column",i,sep=""))
+    cat("\n")
+  }
+  
+  colnames(Data)[which(colnames(Data)==condition_column)]="condition_column"
+  colnames(Data)[which(colnames(Data)==response_column)]="response_column"
+  
+  
+  
+  
+  ###### indices of target parameters in experimental variables
+  target_i=NULL
+  target_columns_renamed=NULL
+  ###### match target parameters
+  for(i in 1:length(target_columns)){
+    cn=colnames(Data)[which(colnames_original==target_columns[i])]
+    target_columns_renamed[i]=cn
+    
+    target_i=c(target_i,as.integer(substr(cn,nchar(cn),nchar(cn))))
+  }
+  
+  
+  
+  
+  ####### run the formula
+  
+    if(length(experimental_columns)==1){
+      lmerFit <- lme4::lmer(response_column ~ condition_column + (1 | experimental_column1), data=Data)
+    }else if(length(experimental_columns)==2){
+      lmerFit <- lme4::lmer(response_column ~ condition_column + (1 | experimental_column1) + (1 | experimental_column2), data=Data)
+    }else if(length(experimental_columns)==3){
+      lmerFit <- lme4::lmer(response_column ~ condition_column + (1 | experimental_column1) + (1 | experimental_column2) + (1 | experimental_column3), data=Data)
+    }else if(length(experimental_columns)==4){
+      lmerFit <- lme4::lmer(response_column ~ condition_column + (1 | experimental_column1) + (1 | experimental_column2) + (1 | experimental_column3) + (1 | experimental_column4), data=Data)
+    }else if(length(experimental_columns)==5){
+      lmerFit <- lme4::lmer(response_column ~ condition_column + (1 | experimental_column1) + (1 | experimental_column2) + (1 | experimental_column3) + (1 | experimental_column4) + (1 | experimental_column5), data=Data)
+    }
+
+  
+  
+  
+  
+  
+  slmerFit <- summary(lmerFit)
+  cat("\n")
+  print("__________________________________________________________________Model statistics:")
+  print(slmerFit)
+  cat("\n")
+  
+  
+}
+
